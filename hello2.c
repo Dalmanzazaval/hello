d96c414d1a4c7a9ed2137de6d509e059e5bc2c9b
Diego Almanza Zavala <dalmanzazaval7@gmail.com>
2019-08-29 11:54:50 -0700

    Update hello2.c

--- hello2.c
+++ hello2.c
@@ -2,7 +2,7 @@
 *Authors Diego Almanza & Henry Buscher
 *Date 2019/29/08
 *
-*Prints names of Authors
+*Prints names of Authors and Majors
 */
 #include<stdlib.h>
 #include<stdio.h>
@@ -10,7 +10,8 @@
 int main(int argc, char **argv) {
 
   printf("Diego Almanza & Henry Buscher\n");
+//added the majors of authors
+  printf("Computer Engineering Majors"/n);
 
-  printf("Computer Engineering"/n);
   return 0;
 }
